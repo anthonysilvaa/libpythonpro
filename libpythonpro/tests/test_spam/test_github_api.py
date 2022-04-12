c8aa37a41768ef423a7aa829e3d2face8017d422
Anthony Silva <hedreson@icloud.com>
2022-04-12 16:29:54 -0300

    Quase lá

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,15 +1,29 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libpythonpro import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/49687791?v=4'
     resp_mock.json.return_value = {
         "login": "anthonysilvaa", "id": 49687791,
-        "avatar_url": "https://avatars.githubusercontent.com/u/49687791?v=4"
+        "avatar_url": url,
     }
-
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('anthonysilvaa')
-    assert 'https://avatars.githubusercontent.com/u/49687791?v=4' == url
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('anthonysilva')
+    assert 'https://avatars.githubusercontent.com/u/3491806?v=4' == url
